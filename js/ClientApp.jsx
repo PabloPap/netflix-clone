f2cb2464f6f713c1e83dcdbd916c660610eb17df
PabloPap <ppapad_33@yahoo.gr>
2018-09-10 13:04:13 +0300

    jsx

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,21 +1,23 @@
-import React from 'react'
-import { render } from 'react-dom'
-
-const ce = React.createElement;
+import React from 'react';
+import { render } from 'react-dom';
 
 const MyTitle = function(props) {
-  return ce('div', null, ce('h1', { style: { color: props.color } }, props.title));
-};
-
-const MyFirstComponent = function() {
-  return ce(
-    'div',
-    { id: 'my-first-component' },
-    ce(MyTitle, { title: 'Game of Thrones', color: 'YellowGreen' }),
-    ce(MyTitle, { title: 'Stranger Things', color: 'GreenYellow' }),
-    ce(MyTitle, { title: 'Rick and Morty', color: 'LimeGreen' }),
-    ce(MyTitle, { title: 'Silicon Valley', color: 'peru' })
+  return (
+    <div>
+      <h1 style={{ color: props.color }}>{props.title}</h1>
+    </div>
   );
 };
 
-render(ce(MyFirstComponent), document.getElementById('app'));
+const MyFirstComponent = function() {
+  return (
+    <div id="my-first-component">
+      <MyTitle title="Game of Thrones" color="YellowGreen" />>
+      <MyTitle title="Stranger Things" color="GreenYellow" />
+      <MyTitle title="Rick and Morty" color="LimeGreen" />
+      <MyTitle title="Silicon Valley" color="peru" />
+    </div>
+  );
+};
+
+render(<MyFirstComponent />, document.getElementById('app'));
