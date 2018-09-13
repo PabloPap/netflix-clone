1b59025385ab83eff45a976c2c38d5521f9476d1
PabloPap <ppapad_33@yahoo.gr>
2018-09-13 13:27:41 +0300

    props and states

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ShowCard from './ShowCard';
 import preload from '../data.json';
 
-const search = () => (
-  <div className="search">
-    {/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> */}
-    {preload.shows.map(show => <h3>{show.title}</h3>)}
-  </div>
-);
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: 'this is some sort of debug statement'
+    };
 
-export default search;
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+  }
+  handleSearchTermChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+  render() {
+    return (
+      <div className="search">
+        {/* <pre><code>{JSON.stringify(preload, null, 4)}</code></pre> */}
+        <header>
+          <h1>svideo</h1>
+          <input
+            onChange={this.handleSearchTermChange}
+            value={this.state.searchTerm}
+            type="text"
+            placeholder="Search"
+          />
+        </header>
+        <div>
+          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
---
+++ js/ShowCard.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { string } from 'prop-types';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width:32%;
+  border: 2px solid #333;
+  border-radius: 4px;
+  margin-bottom: 25px;
+  padding-right: 10px;
+  overflow: hidden;
+`;
+
+const Image = styled.img`
+  width:46%;
+  float: left;
+  margin-right: 10px;
+`;
+
+const ShowCard = props => (
+  <Wrapper>
+    <Image src={`/public/img/posters/${props.poster}`} alt={`${props.title} Show Poster`} />
+    <div>
+      <h3>{props.title}</h3>
+      <h4>({props.year})</h4>
+      <p>{props.description}</p>
+      {/* <p>{props.foo}</p> */}
+    </div>
+  </Wrapper>
+);
+
+// ShowCard.defaultProps = {
+//   foo: 'stuff'
+// };
+
+ShowCard.propTypes = {
+  poster: string.isRequired,
+  title: string.isRequired,
+  year: string.isRequired,
+  description: string.isRequired
+};
+
+export default ShowCard;
